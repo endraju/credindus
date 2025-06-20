47749e4b5002b5ef295cfbc72b3e351c223d5945
endraju <sunrajk92@gmail.com>
2025-06-20 15:25:10 +0530

    jhjhtyui

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1693157732"
+    serviceWorkerVersion: "2437113142"
   }
 });
